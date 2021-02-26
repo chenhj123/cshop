b3436fa8b5f204ee5905c2e3826c06a5ea31a723
chenhj <chenhj@17uhui.com>
2021-02-26 10:53:44 +0800

    修复整合SpringSecurity和JWT实现认证和授权的bug

--- src/main/java/com/chenhj/cshop/tiny/controller/UmsAdminController.java
+++ src/main/java/com/chenhj/cshop/tiny/controller/UmsAdminController.java
@@ -1,7 +1,9 @@
 package com.chenhj.cshop.tiny.controller;
 
 import com.chenhj.cshop.tiny.common.api.CommonResult;
+import com.chenhj.cshop.tiny.dto.UmsAdminLoginParam;
 import com.chenhj.cshop.tiny.mbg.model.UmsAdmin;
+import com.chenhj.cshop.tiny.mbg.model.UmsPermission;
 import com.chenhj.cshop.tiny.service.UmsAdminService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -10,6 +12,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 后台用户管理
@@ -36,5 +42,26 @@ public class UmsAdminController {
         return CommonResult.success(umsAdmin);
     }
 
-//    public CommonResult login(@RequestBody UmsAdminL)
+    @ApiOperation(value = "登录以后返回token")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login(@RequestBody UmsAdminLoginParam umsAdminLoginParam, BindingResult result){
+        String token = adminService.login(umsAdminLoginParam.getUsername(),umsAdminLoginParam.getPassword());
+        if (token == null){
+            return CommonResult.validateFailed("用户名或密码错误");
+        }
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        tokenMap.put("tokenHead", tokenHead);
+        return CommonResult.success(tokenMap);
+    }
+
+    @ApiOperation("获取用户所有权限(包括+-权限)")
+    @RequestMapping(value = "/permission/{adminId}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<UmsPermission>> getPermissionList(@PathVariable Long adminId){
+        List<UmsPermission> permissionList = adminService.getPermissionList(adminId);
+        System.out.println(permissionList);
+        return CommonResult.success(permissionList);
+    }
 }
--- src/test/java/com/chenhj/cshop/tiny/CshopApplicationTests.java
+++ src/test/java/com/chenhj/cshop/tiny/CshopApplicationTests.java
@@ -1,6 +1,6 @@
 package com.chenhj.cshop.tiny;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
